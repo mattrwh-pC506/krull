3b755e6bc22bcfb5fc131dafef5da6304e5443b6
Matthew Whitt <mwhitt.w@gmail.com>
2016-06-06 18:42:14 -0400

    clean up tests and readme

--- krull/tests.py
+++ krull/tests.py
@@ -4,25 +4,25 @@ from server import run_server
 
 @endpoint(path='/users/<int:id>', method='GET')
 def getusers(req, res):
-    res.status = 200
     user_id = req.path_params["id"]
     res.data = {"message": "Hello world, number {}!".format(user_id)}
+    res.status = 200
     return res
 
 
 @endpoint(path='/users/<str:username>', method='GET')
 def getuserbyusername(req, res):
-    res.status = 200
     username = req.path_params["username"]
     res.data = {"message": "Hello world, and hey {}!".format(username)}
+    res.status = 200
     return res
 
 
 @endpoint(path='/users', method='POST')
 def postusers(req, res):
     # do something with data
-    res.status = 201
     res.data = {"message": "success!"}
+    res.status = 201
     return res
 
 
