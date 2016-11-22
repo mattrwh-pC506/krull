5e1a4b87656da92987daf4f59977bef2549a6452
Matthew Whitt <mwhitt.w@gmail.com>
2016-11-21 20:50:12 -0800

    update pip package

---
+++ UPDATE.md
@@ -0,0 +1,7 @@
+#Krull Pip Package Update Instructions
+=======================
+
+[] Update version number in setup.py
+[] Create new distribution package with `python setup.py sdist`
+[] Upload new package pypi with `twine upload dist/DIST_FILE_NAME`
+
--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='krull',
 
     version='0.0.7',
-
+    long_description="",
     description='This is a lightweight microframework for building restful APIs',
-    long_description=long_description,
 
     url='https://github.com/mattrwh/krull',
 
