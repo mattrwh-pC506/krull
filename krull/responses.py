a73401122b6d1024f662fab41c6b92c62911487a
Matthew Whitt <mwhitt.w@gmail.com>
2016-11-24 14:46:45 -0800

    Add http template response type

--- krull/responses.py
+++ krull/responses.py
@@ -15,3 +15,15 @@ class HttpResponse(Response):
     def __init__(self, data, *args, **kwargs):
         kwargs["mimetype"] = "text/html"
         return super().__init__(data, *args, **kwargs)
+
+
+class HttpTemplateResponse(Response):
+    
+    def __init__(self, filepath, *args, **kwargs):
+        kwargs["mimetype"] = "text/html"
+
+        with open(filepath) as f:
+            fileraw = f.read()
+        
+        data = str(fileraw)
+        return super().__init__(data, *args, **kwargs)
